2b7c9f692a13de250c71bb1ccbc0dc81e117c07e
ART Raymond <rebelsjustbecause@gmail.com>
2025-12-02 22:19:54 +0100

    style: format code and improve consistency across files

--- ap-portfolio/src/App.jsx
+++ ap-portfolio/src/App.jsx
@@ -1,38 +1,41 @@
 
 import './App.css'
 
-import NavBar from './components/navbar/NavBar'
-import HeroMain from './components/heroSection/HeroMain'
-import HeroGradient from './components/heroSection/HeroGradient'
-import SubHeroMain from './components/subHeroSection/SubHeroMain'
+import NavBar  from './components/navbar/NavBar'
 
-import AboutMeMain from './components/aboutMeSection/AboutMeMain'
-import SkillsMain from './components/skillsSection/SkillsMain'
-import SubSkills from './components/skillsSection/SubSkills'
-import ExperienceMain from './components/experienceSection/ExperienceMain'
-import ProjectsMain from './components/projectSection/ProjectsMain'
-import ContactMeMain from './components/contactMeSection/ContactMeMain'
-import FooterMain from './components/footer/FooterMain'
+import HeroMain  from './components/heroSection/HeroMain'
+
+import HeroGradient  from './components/heroSection/HeroGradient'
+
+import SubHeroMain from  './components/subHeroSection/SubHeroMain'
+
+import AboutMeMain from  './components/aboutMeSection/AboutMeMain'
+import SkillsMain from  './components/skillsSection/SkillsMain'
+import SubSkills from  './components/skillsSection/SubSkills'
+import ExperienceMain  from './components/experienceSection/ExperienceMain'
+import ProjectsMain from  './components/projectSection/ProjectsMain'
+import ContactMeMain from  './components/contactMeSection/ContactMeMain'
+import FooterMain  from './components/footer/FooterMain'
 
 
-function App() {
+function  App() {
 
 
-  return (
+  return  (
     <>
-      <div>
-        <NavBar />
-        <HeroMain />
-        <HeroGradient />
+      <div> 
+        <NavBar/>
+        <HeroMain/>
+        <HeroGradient/>
         <SubHeroMain />
-        <AboutMeMain />
-        <SkillsMain />
-        <SubSkills/>
-        <ExperienceMain/>
-        <ProjectsMain/>
-        <ContactMeMain/>
-        <FooterMain/>
-      </div>
+        <AboutMeMain/>
+        <SkillsMain/>
+        <SubSkills />
+        <ExperienceMain />
+        <ProjectsMain />
+        <ContactMeMain />
+        <FooterMain />
+      </div> 
     </>
   )
 }
