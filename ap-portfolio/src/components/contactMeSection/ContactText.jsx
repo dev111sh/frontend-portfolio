2305351d8c518579eb6ff3389ca88aada8d27fa8
ART Raymond <rebelsjustbecause@gmail.com>
2025-12-03 09:34:10 +0100

    fix: Contact text message

--- ap-portfolio/src/components/contactMeSection/ContactText.jsx
+++ ap-portfolio/src/components/contactMeSection/ContactText.jsx
@@ -1,13 +1,12 @@
-const ContactText = () => {
-    return (
-      <div>
-        <h2 className="text-orange text-3xl mb-4">Get In Touch</h2>
-        <p className="text-white text-lg">
-          Hit me up if you’d like to collaborate, discuss your project, or just
-          say hello. < br/> I’d love to hear from you!
+const ContactText = () => { 
+    return ( 
+      <div> 
+        <h2 className="text-orange text-3xl mb-4">Get In Touch </h2> 
+        <p className="text-white text-lg"> 
+          Hit me up if you’d like to work, collaborate, discuss your project, or just say hello. < br/> I’d love to hear from you!
         </p>
       </div>
     );
   };
-  
-  export default ContactText;
+   
+  export default ContactText; 
